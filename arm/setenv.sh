d034568c9d859c19dfb4a98793a618e7b7e7881f
Sebastian Raubach <sebastian.raubach@hutton.ac.uk>
2020-03-16 13:53:27 +0000

    - CHG: Adjusted build process to changed requirements.

--- arm/setenv.sh
+++ arm/setenv.sh
@@ -5,4 +5,4 @@ let heapKB=$totalMemKB*$usagePercent/100
 let heapMB=$heapKB/1024
 let max=$(( $heapMB > 512 ? $heapMB : 512 ))
 export CATALINA_OPTS="$CATALINA_OPTS -Xmx${heapMB}m"
-export JAVA_OPTS="$CATALINA_OPTS -Xmx${heapMB}m"
+export JAVA_OPTS="$JAVA_OPTS -Xmx${heapMB}m"
